81acb50fbdbc0eb3efd7c3d9f344a6ac579ae0ae
Csaba Nagy <csaba.nagy@mapp.com>
2018-10-09 12:48:47 +0200

    structure view: add editor parameter to the model constructor (necessary for auto-scroll from source code)

--- src/main/java/com/javampire/openscad/OpenSCADStructureViewFactory.java
+++ src/main/java/com/javampire/openscad/OpenSCADStructureViewFactory.java
@@ -1,10 +1,13 @@
 package com.javampire.openscad;
 
-import com.intellij.ide.structureView.*;
+import com.intellij.ide.structureView.StructureViewBuilder;
+import com.intellij.ide.structureView.StructureViewModel;
+import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class OpenSCADStructureViewFactory implements PsiStructureViewFactory {
     @Nullable
@@ -14,7 +17,7 @@ public class OpenSCADStructureViewFactory implements PsiStructureViewFactory {
             @NotNull
             @Override
             public StructureViewModel createStructureViewModel(@Nullable Editor editor) {
-                return new OpenSCADStructureViewModel(psiFile);
+                return new OpenSCADStructureViewModel(psiFile, editor);
             }
         };
     }
--- src/main/java/com/javampire/openscad/OpenSCADStructureViewModel.java
+++ src/main/java/com/javampire/openscad/OpenSCADStructureViewModel.java
@@ -5,10 +5,9 @@ import com.intellij.ide.structureView.StructureViewModelBase;
 import com.intellij.ide.structureView.StructureViewTreeElement;
 import com.intellij.ide.util.treeView.smartTree.Grouper;
 import com.intellij.ide.util.treeView.smartTree.Sorter;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import com.javampire.openscad.psi.OpenSCADFile;
-import com.javampire.openscad.psi.impl.OpenSCADFunctionDeclarationImpl;
-import com.javampire.openscad.psi.impl.OpenSCADModuleDeclarationImpl;
+import com.javampire.openscad.psi.*;
 import org.fest.util.Arrays;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,20 +15,16 @@ public class OpenSCADStructureViewModel
         extends StructureViewModelBase
         implements StructureViewModel.ElementInfoProvider
 {
-    public OpenSCADStructureViewModel(PsiFile psiFile) {
-        super(psiFile, new OpenSCADStructureViewElement(psiFile));
-        // TODO: see why this doesn't work:
+    public OpenSCADStructureViewModel(PsiFile psiFile, Editor editor) {
+        super(psiFile, editor, new OpenSCADStructureViewElement(psiFile));
         withSuitableClasses(
-                OpenSCADModuleDeclarationImpl.class,
-                OpenSCADFunctionDeclarationImpl.class
+                OpenSCADModuleDeclaration.class,
+                OpenSCADFunctionDeclaration.class,
+                OpenSCADUseItem.class,
+                OpenSCADIncludeItem.class
         );
+        withSorters(Sorter.ALPHA_SORTER);
     }
-
-    @NotNull
-    public Sorter[] getSorters() {
-        return new Sorter[]{Sorter.ALPHA_SORTER};
-    }
-
 
     @Override
     public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
