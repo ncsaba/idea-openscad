68f251ff177d83b0e7340d82404eed7b53c56b63
Vova Stelmashchuk <vovochkastelmashchuk@gmail.com>
2020-05-07 00:25:39 +0300

    Small code refactor

--- src/main/java/com/javampire/openscad/color/OpenSCADColorProvider.java
+++ src/main/java/com/javampire/openscad/color/OpenSCADColorProvider.java
@@ -8,7 +8,6 @@ import com.intellij.xml.util.ColorMap;
 import com.javampire.openscad.OpenSCADFileType;
 import com.javampire.openscad.psi.OpenSCADTypes;
 import com.javampire.openscad.psi.impl.OpenSCADBuiltinObjImpl;
-import com.javampire.openscad.psi.impl.OpenSCADBuiltinOpImpl;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -20,28 +19,35 @@ class OpenSCADColorProvider implements ElementColorProvider {
     @Nullable
     @Override
     public Color getColorFrom(@NotNull PsiElement element) {
-        if (element.getNode().getElementType() == OpenSCADTypes.STRING_LITERAL) {
-            final PsiElement colorParent = PsiTreeUtil.getParentOfType(element, OpenSCADBuiltinOpImpl.class);
-            if (colorParent != null && colorParent.getText().startsWith("color")) {
-                final String colorDef = element.getText();
-                return ColorMap.getColor(colorDef.substring(1, colorDef.length() - 1));
-            }
+        final PsiElement fullColorBlock = getOpenSCADBuiltinObj(element);
+        if (fullColorBlock != null) {
+            final String colorDef = element.getText();
+            return ColorMap.getColor(colorDef.substring(1, colorDef.length() - 1));
         }
+
         return null;
     }
 
     @Override
     public void setColorTo(@NotNull PsiElement element, @NotNull Color color) {
-        if (element.getNode().getElementType() == OpenSCADTypes.STRING_LITERAL) {
-            final PsiElement colorBlockWithBody = PsiTreeUtil.getParentOfType(element, OpenSCADBuiltinObjImpl.class);
-            if (colorBlockWithBody != null) {
-                final String newText = createColorHex(color) + colorBlockWithBody.getChildren()[1].getText();
+        final PsiElement fullColorBlock = getOpenSCADBuiltinObj(element);
+        if (fullColorBlock != null) {
+            final String newText = createColorHex(color) + fullColorBlock.getChildren()[1].getText();
 
-                final PsiElement newElement = PsiFileFactory.getInstance(element.getProject())
-                        .createFileFromText("tmp.scad", OpenSCADFileType.INSTANCE, newText).getFirstChild();
-                colorBlockWithBody.replace(newElement);
+            final PsiElement newElement = PsiFileFactory.getInstance(element.getProject())
+                    .createFileFromText("tmp.scad", OpenSCADFileType.INSTANCE, newText).getFirstChild();
+            fullColorBlock.replace(newElement);
+        }
+    }
+
+    private OpenSCADBuiltinObjImpl getOpenSCADBuiltinObj(PsiElement element) {
+        if (element.getNode().getElementType() == OpenSCADTypes.STRING_LITERAL) {
+            final OpenSCADBuiltinObjImpl fullColorBlock = PsiTreeUtil.getParentOfType(element, OpenSCADBuiltinObjImpl.class);
+            if (fullColorBlock != null && fullColorBlock.getText().startsWith("color")) {
+                return fullColorBlock;
             }
         }
+        return null;
     }
 
     private String createColorHex(Color color) {
